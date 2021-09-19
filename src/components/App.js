d13913b8fd4e07c5f207d665b85be47aecdadb18
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-19 15:34:34 +0530

    Created a Header component to display a navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,22 @@ import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 
+import Header from './Header';
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" component={StreamCreate} />
-                <Route path="/streams/show" component={StreamShow} />
-                <Route path="/streams/edit" component={StreamEdit} />
-                <Route path="/streams/delete" component={StreamDelete} />
-            </div>
-        </BrowserRouter>
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/show" component={StreamShow} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
