883b3c801c4672c0a71518e0531929da10e8b544
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-20 20:17:07 +0530

    Hooked up authReducer reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 
 export default combineReducers({
     // Add additional reducers here
-    replaceMe: () => 'asdfgh'
+    auth: authReducer
 });
