2bc1abbf062092490a3cee0e2c7b97c19d6cf1f8
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-22 19:19:00 +0530

    Hooked up redux-from reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
     // Add additional reducers here
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
