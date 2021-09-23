4dc2b83dc100d5ebd4f3174dc4d9d5212b71028e
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-23 19:54:46 +0530

    Created buttons that allow owner to edit/delete a stream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,10 +8,23 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdminAccess(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+
+            );
+        }
+    }
+
     renderListOfStreams() {
         return this.props.streams.map(stream => {
             return (
                 <div key={stream.id} className="item">
+                    {this.renderAdminAccess(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -22,6 +35,16 @@ class StreamList extends React.Component {
         });
     }
 
+    // renderCreateStreamButton() {
+    //     if (this.props.isSignedIn) {
+    //         return (
+    //             <div style={{ textAlign: 'right' }}>
+    //                 <button className="ui button secondary">Create Stream</button>
+    //             </div>
+    //         );
+    //     }
+    // }
+
     render() {
         return (
             <div>
@@ -29,6 +52,7 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderListOfStreams()}
                 </div>
+                {/* {this.renderCreateStreamButton()} */}
             </div>
         );
     }
@@ -36,7 +60,9 @@ class StreamList extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        streams: Object.values(state.streams)
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+        // isSignedIn: state.auth.isSignedIn
     };
 };
 
