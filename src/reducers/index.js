37741dba7b3bceed8daf0d89bff6274ef659f661
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-23 19:05:46 +0530

    Hooked up streamReducer reducer to the main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     // Add additional reducers here
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
