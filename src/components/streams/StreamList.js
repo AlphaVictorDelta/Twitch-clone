6fba24698a04abc18ff0d720564feec3414d7824
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-23 20:19:54 +0530

    Created a button to navigate user to the create streams page

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -15,7 +16,6 @@ class StreamList extends React.Component {
                     <button className="ui button primary">Edit</button>
                     <button className="ui button negative">Delete</button>
                 </div>
-
             );
         }
     }
@@ -35,15 +35,15 @@ class StreamList extends React.Component {
         });
     }
 
-    // renderCreateStreamButton() {
-    //     if (this.props.isSignedIn) {
-    //         return (
-    //             <div style={{ textAlign: 'right' }}>
-    //                 <button className="ui button secondary">Create Stream</button>
-    //             </div>
-    //         );
-    //     }
-    // }
+    renderCreateStreamButton() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button secondary">Create Stream</Link>
+                </div>
+            );
+        }
+    }
 
     render() {
         return (
@@ -52,7 +52,7 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderListOfStreams()}
                 </div>
-                {/* {this.renderCreateStreamButton()} */}
+                {this.renderCreateStreamButton()}
             </div>
         );
     }
@@ -61,8 +61,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
     return {
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId
-        // isSignedIn: state.auth.isSignedIn
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     };
 };
 
