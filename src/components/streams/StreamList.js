0640af13778a4072f28d104ca99cf63fe29422c9
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-23 19:28:29 +0530

    Modified the StreamList component to fetch and display streams from redux store

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = (props) => {
-    return <div>StreamList</div>
+class StreamList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderListOfStreams() {
+        return this.props.streams.map(stream => {
+            return (
+                <div key={stream.id} className="item">
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Available Streams</h2>
+                <div className="ui celled list">
+                    {this.renderListOfStreams()}
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        streams: Object.values(state.streams)
+    };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams: fetchStreams })(StreamList);
